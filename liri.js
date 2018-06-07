f5b700d274529fe6a1f00ca1683ff5d78753381c
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-06 23:47:01 -0700

    spotify and movie working

--- liri.js
+++ liri.js
@@ -7,68 +7,81 @@ const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
 // const client = new Twitter(keys.twitter);
 
-
-
-
-
-
-
+var songArr = [];
+var song = 'The Sign';
 let action = process.argv[2];
-let arg3 = process.argv[3];
+//Check length of arguments and takes input and joins.
+if (process.argv.length > 3) {
+  process.argv.shift(); process.argv.shift(); process.argv.shift();
+  for (let i = 0; i < process.argv.length; i++) {
+    songArr.push(process.argv[i]);
+  }
+  song = songArr.join(' ');
+}
 //switch for inputs placed in function
-
-  switch (action) {
+switch (action) {
   case "my-tweets":
     myTweets();
     break;
 
   case "spotify-this-song":
-    mySpotify(arg3);
+    mySpotify(song);
     break;
 
   case "movie-this":
-    myMovie();
+    myMovie(song);
     break;
 
   case "do-what-it-says":
     myWhat();
     break;
-  default: 
+  default:
     console.log("Invalid input.");
     break;
-  }
-
+}
 //functions for inputs
-function myTweets(){
-    console.log('tweets go here');
+function myTweets() {
+  console.log('tweets go here');
 }
-function mySpotify(song){
-    if (song === undefined){
-      song = 'The Sign';
+function mySpotify(song) {
+  spotify.search({ type: 'track', query: song }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    } else
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+    console.log("Album: " + data.tracks.items[0].album.name);
+    console.log("Song: " + data.tracks.items[0].name);
+    console.log("Spotify Link: " + data.tracks.items[0].external_urls.spotify);
+  });
+}
+function myMovie(movie) {
+  if (song === 'The Sign') {
+    song = 'Mr. Nobody.';
+  }
+  request("https://www.omdbapi.com/?t=" + song + "&y=&plot=short&tomatoes=true&apikey=trilogy", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      let movie = JSON.parse(body);
+      console.log("Title: " + movie.Title);
+      console.log("Year: " + movie.Year);
+      console.log("IMDB Rating: " + movie.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + movie.tomatoUserRating);
+      console.log("Country: " + movie.Country);
+      console.log("Language: " + movie.Language);
+      console.log("Plot: " + movie.Plot);
+      console.log("Actors: " + movie.Actors);
+      fs.appendFile("log.txt", "Title: " + movie.Title + "\n" + "Year: " + movie.Year + "\n" + "IMDB Rating: " + movie.imdbRating + "\n" + "Rotten Tomatoes Rating: " + movie.tomatoUserRating + "\n" + "Country: " + movie.Country + "\n" + "Language: " + movie.Language + "\n" + "Plot: " + movie.Plot + "\n" + "Actors: " + movie.Actors + "\n=======================================================================\n");
     }
-
-    spotify.search({ type : 'track', query: song }, function(err, data) {
-      if (err) {
-        return console.log('Error occurred: ' + err);
-      } else 
-        console.log(data); 
-     });
+    else {  
+      console.log(error);
+    }
+  });
 }
-function myMovie(){
-    console.log('movies go here');
+function myWhat() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    var doSplit = data.split(",");
+    mySpotify(doSplit[1]);
+  });
 }
-function myWhat(){
-    fs.readFile("random.txt", "utf8", function(error, data){
-      if (error) {
-        return console.log(error);
-      }
-      var doSplit = data.split(",");
-      console.log(doSplit);
-      mySpotify(doSplit[1]);
-    });
-}
-
-//FOR BONUS
-//===================
-// fs.appendFile(textFile, "Hello Kitty", function(err) {
-// });
